4f61d60b92b96f59a3d6ed29b632f3f1abac1303
Jinho <wjs9280@naver.com>
2017-09-27 18:53:26 +0900

    동기화

---
+++ tiles/grasswater.tsx
@@ -0,0 +1,29 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="grasswater" tilewidth="16" tileheight="16" tilecount="18" columns="3">
+ <image source="../sooyeon/Assets/Images/tiles/grasswater.png" width="48" height="96"/>
+ <terraintypes>
+  <terrain name="watergrass" tile="13"/>
+ </terraintypes>
+ <tile id="0" terrain=",,,0"/>
+ <tile id="1" terrain=",,0,0"/>
+ <tile id="2" terrain=",,0,"/>
+ <tile id="3" terrain=",0,,0"/>
+ <tile id="4" terrain="0,0,0,0"/>
+ <tile id="5" terrain="0,,0,"/>
+ <tile id="6" terrain=",0,,"/>
+ <tile id="7" terrain="0,0,,"/>
+ <tile id="8" terrain="0,,,"/>
+ <tile id="9" terrain="0,0,0,"/>
+ <tile id="10" terrain="0,0,,0"/>
+ <tile id="12" terrain="0,,0,0"/>
+ <tile id="13" terrain=",0,0,0"/>
+ <tile id="14" terrain="0,0,0,0"/>
+ <tile id="17" terrain="0,0,0,0">
+  <animation>
+   <frame tileid="15" duration="100"/>
+   <frame tileid="16" duration="100"/>
+   <frame tileid="17" duration="100"/>
+   <frame tileid="15" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
---
+++ tiles/untitled.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="deco" tilewidth="16" tileheight="16" tilecount="18" columns="3">
+ <image source="../sooyeon/Assets/Images/tiles/deco.png" width="48" height="96"/>
+ <terraintypes>
+  <terrain name="deco" tile="0"/>
+ </terraintypes>
+ <tile id="0" terrain=",0,,0"/>
+ <tile id="1" terrain="0,0,0,0"/>
+ <tile id="2" terrain="0,,0,"/>
+ <tile id="3" terrain=",,0,0"/>
+ <tile id="6" terrain="0,0,0,0"/>
+ <tile id="9" terrain="0,0,,"/>
+</tileset>
